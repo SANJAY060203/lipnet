746373862fa57c93113900b9a48b7f254b7c6499
haidun.li <kaiton.ri@gmail.com>
2024-04-10 03:26:54 -0300

    Refactor modelutil.py: Import Keras layers and models, update load_model function

--- app/modelutil.py
+++ app/modelutil.py
@@ -1,8 +1,11 @@
 import os
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv3D, LSTM, Dense, Dropout, Bidirectional, MaxPool3D, Activation, Reshape, SpatialDropout3D, BatchNormalization, TimeDistributed, Flatten
+from keras.layers import Conv3D, LSTM, Dense, Dropout, Bidirectional, MaxPool3D, Activation, Reshape, SpatialDropout3D, BatchNormalization, TimeDistributed, Flatten
+from keras.models import Sequential
 from keras.layers import LSTM, Bidirectional
 from keras.initializers import Orthogonal
+import tensorflow as tf
 
 def load_model() -> Sequential:
     model = Sequential()
@@ -31,6 +34,25 @@ def load_model() -> Sequential:
 
     model.add(Dense(41, kernel_initializer='he_normal', activation='softmax'))
 
-    model.load_weights(os.path.join('..','models','checkpoint'))
+    # weights_path = os.path.join('..', 'models', 'checkpoint.h5')  # Adjust the extension as necessary
+    # # weights_path = 'C:/haidun.li/NSCC/LipNet/models/checkpoint'
+    # print('weights_path:', weights_path)
+    # if os.path.exists(weights_path):
+    #     model.load_weights(weights_path)
+    # else:
+    #     raise FileNotFoundError(f"Weight file {weights_path} does not exist.")
+
+    # return model
+
+     # Create a checkpoint instance that points to the folder where the checkpoints are saved
+    checkpoint_dir = os.path.join('..', 'models')
+    checkpoint = tf.train.Checkpoint(model=model)
+
+    # Restore the latest checkpoint
+    latest = tf.train.latest_checkpoint(checkpoint_dir)
+    if latest:
+        checkpoint.restore(latest)
+    else:
+        raise FileNotFoundError("No checkpoint found in {}".format(checkpoint_dir))
 
     return model
